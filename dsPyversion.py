95aba1ad270d8100853d7c404f5f8d36c10f2f5c
Delta-11 <exdelta11@gmail.com>
2025-01-26 20:50:54 +0530

    Sentiment and Info extract added

--- dsPyversion.py
+++ dsPyversion.py
@@ -4,23 +4,26 @@ from typing import Literal
 lm = dspy.LM('ollama_chat/deepseek-r1:1.5b', api_base='http://localhost:11434', api_key='')
 dspy.configure(lm=lm)
 
+# Normal Prompting
 response = lm("How many number of R is there in word Strawberry?")
 print("Response: ", response)
 # print(len(lm.history))
 # print(lm.history)
 
-
+# THis is RAG search with colbert
 def search(query: str) -> list[str]:
-    results = dspy.ColBERTv2(url='./README.md')(query, k=3)
+    results = dspy.ColBERTv2(url='http://20.102.90.50:2017/wiki17_abstracts')(query, k=3)
     return [x['text'] for x in results]
 
 rag = dspy.ChainOfThought('context, question -> response')
 
-question = "What's the introduction of this book?"
-response2 =rag(context=search(question), question=question)
-print("Response: ", response2)
+question = "What's the name of the castle that David Gregory inherited?"
+
+rag(context=search(question), question=question)
 
 
+# This is for sentiment analysis
+questionsList = ["Are you out of your mind?", "He didn't deliver on his part.", "She gave a great job talk at the conference.", "The project was challenging."]
 
 class Classify(dspy.Signature):
     """Classify sentiment of a given sentence."""
@@ -30,4 +33,28 @@ class Classify(dspy.Signature):
     confidence: float = dspy.OutputField()
 
 classify = dspy.Predict(Classify)
-classify(sentence="This book was super fun to read, though not the last chapter.")
+for sentence in questionsList:
+    response = classify(sentence=sentence)
+    print(f"Raw Response for {sentence}: {response}")
+    print(f"Response for {sentence}: {response}\n")
+
+
+
+class ExtractInfo(dspy.Signature):
+    """Extract structured information from text."""
+
+    text: str = dspy.InputField()
+    knowledge: str = dspy.OutputField()
+    diverse: list[str] = dspy.OutputField()
+    impact: list[str] = dspy.OutputField()
+    entities: list[dict[str, str]] = dspy.OutputField(desc="a list of entities and their metadata")
+
+module = dspy.Predict(ExtractInfo)
+
+text = "Whether it's a scientific breakthrough, a thought-provoking piece of art, or the latest technological innovation, there is always something fascinating to explore. With each passing day, we uncover new discoveries and push the boundaries of human knowledge. From the intricacies of the natural world to the vast depths of outer space, every subject offers a unique opportunity for exploration and understanding. Through curiosity and perseverance, we continue to expand our horizons and shape the world we live in. The possibilities are endless, and the journey of discovery never ceases."
+response = module(text=text)
+
+print(response.knowledge)
+print(response.diverse)
+print(response.impact)
+print(response.entities)
